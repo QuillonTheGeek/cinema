e9feb5d982ca11df57a37d0eec5d954c1c19414e
Quillon <quillonthegeek@gmail.com>
2022-09-06 09:53:03 -0700

    registering route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Movies from "./component/movies";
 import Customers from "./component/customers";
 import Rentals from "./component/rentals";
@@ -9,10 +9,14 @@ import NotFound from "./component/notfound";
 function App() {
   return (
     <main className="container">
-      <Route path="/movies" component={Movies} />
-      <Route path="/customers" component={Customers} />
-      <Route path="/rentals" component={Rentals} />
-      <Route path="/not-found" component={NotFound} />
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={Rentals} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
     </main>
   );
 }
