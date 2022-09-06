ba7553322ad26e3076da5c82314cc9b7e9755b78
Quillon <quillonthegeek@gmail.com>
2022-09-06 09:47:16 -0700

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route } from "react-router-dom";
 import Movies from "./component/movies";
+import Customers from "./component/customers";
+import Rentals from "./component/rentals";
+import NotFound from "./component/notfound";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Route path="/movies" component={Movies} />
+      <Route path="/customers" component={Customers} />
+      <Route path="/rentals" component={Rentals} />
+      <Route path="/not-found" component={NotFound} />
     </main>
   );
 }
---
+++ src/component/movieForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieForms = () => {
+  return <h1>MovieForms</h1>;
+};
+
+export default MovieForms;
