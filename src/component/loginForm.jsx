6003a23e4a5eeba8f6238a704228a6809285177d
Quillon <quillonthegeek@gmail.com>
2022-09-07 03:17:36 -0700

    Validating

--- src/component/loginForm.jsx
+++ src/component/loginForm.jsx
@@ -4,16 +4,27 @@ import Input from "./common/input";
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
+    errors: {},
   };
-  username = React.createRef();
 
-  //   componentDidMount() {
-  //     this.username.current.focus();
-  //   }
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
 
-    const username = this.username.current.value;
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
 
     console.log("summit");
   };
@@ -25,25 +36,28 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
         <h1>Login Form</h1>
-        <Input
-          name="username"
-          value={account.username}
-          label="Username"
-          onChange={this.handleChange}
-        />
-        <Input
-          name="password"
-          value={account.password}
-          label="Password"
-          onChange={this.handleChange}
-        />
-
-        <button className="btn btn-primary ">Login</button>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+            error={errors.username}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+            error={errors.password}
+          />
+          <button className="btn btn-primary ">Login</button>
+        </form>
       </div>
     );
   }
